fa24566dc21dde631e5d3a212cb273c079a45648
Sergey Petrov <sekk1e@yandex.ru>
2020-09-16 22:32:17 +0700

    + defer of full state stream calculation

--- src/App/pages/Index/Input/lib.ts
+++ src/App/pages/Index/Input/lib.ts
@@ -37,17 +37,17 @@ type EventHandler<FullState, StoredState, Args> = (state: FullState, args: Args)
 
 type FinalController<
   Name extends string,
-FullState,
-Deps extends Dependencies<any, any, any>,
-EventEmitterMap extends Record<string, (args: any) => void>
+  FullState,
+  Deps extends Dependencies<any, any, any>,
+  EventEmitterMap extends Record<string, (args: any) => void>
   > = {
-  getViewInterface(): ControllerViewInterface<FullState, EventEmitterMap>;
-  getParentInterface(): ControllerParentInterface<Name, Deps>;
-}
+    getViewInterface(): ControllerViewInterface<FullState, EventEmitterMap>;
+    getParentInterface(): ControllerParentInterface<Name, Deps>;
+  }
 
 // type ControllerWithStoredState<StoredState> = {
-  // defineName<Name extends string>(): ControllerWithName<Name, StoredState>;
-  // defineDependencies<D extends Dependencies> (): ControllerWithDependencies<Name, StoredState, D, StoredState>;
+// defineName<Name extends string>(): ControllerWithName<Name, StoredState>;
+// defineDependencies<D extends Dependencies> (): ControllerWithDependencies<Name, StoredState, D, StoredState>;
 // }
 
 type Dependencies<State extends Record<string, any>, Events extends Record<string, any>, View> = {
@@ -58,19 +58,19 @@ type Dependencies<State extends Record<string, any>, Events extends Record<strin
 
 type DependencyDefinitionInterface<Name extends string, StoredState, Deps extends Dependencies<any, any, any>> = {
   defineStateDependency<Key extends string, T>
-  (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<Record<Key, T>, {}, {}>>;
+    (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<Record<Key, T>, {}, {}>>;
   // TODO payload could be empty
   defineEventDependency<Key extends string, Payload>
-  (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, Record<Key, Payload>, {}>>;
+    (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, Record<Key, Payload>, {}>>;
   defineViewDependency<Key extends string, Props = {}>
-  (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, {}, Record<Key, Props>>>;
+    (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, {}, Record<Key, Props>>>;
 }
 
 type DerivedStateDefinitionInterface<Name extends string, StoredState, FullState, Deps extends Dependencies<any, any, any>> = {
   defineDerivedState<P extends string, S1, V>(
     property: P, uses: [Use<FullState, S1>], selector: (a1: S1) => V
   ): ControllerWithDerivedState<Name, StoredState, FullState & Record<P, V>, Deps>;
- defineDerivedState<P extends string, S1, S2, V>(
+  defineDerivedState<P extends string, S1, S2, V>(
     property: P, uses: [Use<FullState, S1>, Use<FullState, S1>], selector: (a1: S1, a2: S2) => V
   ): ControllerWithDerivedState<Name, StoredState, FullState & Record<P, V>, Deps>;
 }
@@ -108,9 +108,9 @@ type ControllerWithDependencies<Name extends string, StoredState, Deps extends D
 //   defineDerivedState<P extends string, S1, S2, S3, V>(
 //     property: P, uses: [Use<FullState, S1>, Use<FullState, S2>, Use<FullState, S3>], selector: (a1: S1, a2: S2, a3: S3) => V
 //   ): ControllerWithDependencies<Name, StoredState, D, FullState & Record<P, V>>;
-  // defineEvents<EventList extends Array<Event<string, FullState, StoredState, any>>>(
-  //   getEvents: (args: GetEventsArguments<FullState, StoredState>) => EventList
-  // ): FinalController<FullState, EventEmitterMapOf<EventList>>;
+// defineEvents<EventList extends Array<Event<string, FullState, StoredState, any>>>(
+//   getEvents: (args: GetEventsArguments<FullState, StoredState>) => EventList
+// ): FinalController<FullState, EventEmitterMapOf<EventList>>;
 // }
 
 // type S = {
@@ -120,10 +120,10 @@ type ControllerWithDependencies<Name extends string, StoredState, Deps extends D
 
 type _EventMapOf<T, Acc = {}> =
   T extends []
-    ? Acc
-    : T extends [Event<infer Name, any, any, infer Args>, ...infer XS]
-      ? _EventMapOf<XS, Acc & Record<Name, (args: Args) => void>>
-      : never;
+  ? Acc
+  : T extends [Event<infer Name, any, any, infer Args>, ...infer XS]
+  ? _EventMapOf<XS, Acc & Record<Name, (args: Args) => void>>
+  : never;
 
 type EventEmitterMapOf<List> = List extends Array<infer T>
   ? U.ListOf<T> extends Array<Event<string, any, any, any>> ? _EventMapOf<U.ListOf<T>> : never
@@ -170,9 +170,9 @@ type S = {
 }
 
 makeViewController('test')
-  .defineStoredState<S>({value: ''})
+  .defineStoredState<S>({ value: '' })
   .defineStateDependency()
-;
+  ;
 
 export function makeViewController<Name extends string>(name: Name) {
   return makeControllerWithName();
@@ -187,47 +187,47 @@ export function makeViewController<Name extends string>(name: Name) {
 
   function getDependenciesDefinitionInterface<StoredState, Deps extends Dependencies<any, any, any>>
     (initialStoredState: StoredState): DependencyDefinitionInterface<Name, StoredState, Deps> {
-      return {
-        defineStateDependency<Key extends string, T>
-          (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<Record<Key, T>, {}, {}>> {
-            return makeControllerWithDepedencies(initialStoredState);
-        },
-        defineEventDependency<Key extends string, Payload>
-          (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, Record<Key, Payload>, {}>> {
-            return makeControllerWithDepedencies(initialStoredState);
-          },
-        defineViewDependency<Key extends string, Props = {}>
-          (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, {}, Record<Key, Props>>> {
-            return makeControllerWithDepedencies(initialStoredState);
-          },
-        }
-      };
+    return {
+      defineStateDependency<Key extends string, T>
+        (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<Record<Key, T>, {}, {}>> {
+        return makeControllerWithDepedencies(initialStoredState);
+      },
+      defineEventDependency<Key extends string, Payload>
+        (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, Record<Key, Payload>, {}>> {
+        return makeControllerWithDepedencies(initialStoredState);
+      },
+      defineViewDependency<Key extends string, Props = {}>
+        (): ControllerWithDependencies<Name, StoredState, Deps & Dependencies<{}, {}, Record<Key, Props>>> {
+        return makeControllerWithDepedencies(initialStoredState);
+      },
+    }
+  };
 
 
   function getDerivedStateDefinitionInterface<StoredState, FullState, Deps extends Dependencies<any, any, any>>(
     initialStoredState: StoredState, derivedStateDescription: DerivedStateDescription[]
   ): DerivedStateDefinitionInterface<Name, StoredState, FullState, Deps> {
-      return {
-        defineDerivedState<P extends string, V>(
-          property: P, uses: Array<Use<FullState, any>>, selector: (...args: any[]) => V
-        ): ControllerWithDerivedState<Name, StoredState, FullState & Record<P, V>, Deps> {
-          return makeContollerWithDerivedState(initialStoredState, [...derivedStateDescription, { property, uses, selector }])
-        }
+    return {
+      defineDerivedState<P extends string, V>(
+        property: P, uses: Array<Use<FullState, any>>, selector: (...args: any[]) => V
+      ): ControllerWithDerivedState<Name, StoredState, FullState & Record<P, V>, Deps> {
+        return makeContollerWithDerivedState(initialStoredState, [...derivedStateDescription, { property, uses, selector }])
       }
     }
+  }
 
   function getEventsDefinitionInterface<StoredState, FullState, Deps extends Dependencies<any, any, any>>(
     initialStoredState: StoredState,
     derivedStateDescription: DerivedStateDescription[]
   ): EventsDefinitionInterface<Name, StoredState, FullState, Deps> {
-      return {
-        defineEvents<EventList extends Array<Event<string, FullState, StoredState, any>>>(
-          getEvents: (args: GetEventsArguments<FullState, StoredState>) => EventList
-        ): FinalController<Name, FullState, Deps, EventEmitterMapOf<EventList>> {
-          return makeFinalController(initialStoredState, derivedStateDescription, getEvents);
-        }
-      };
-    }
+    return {
+      defineEvents<EventList extends Array<Event<string, FullState, StoredState, any>>>(
+        getEvents: (args: GetEventsArguments<FullState, StoredState>) => EventList
+      ): FinalController<Name, FullState, Deps, EventEmitterMapOf<EventList>> {
+        return makeFinalController(initialStoredState, derivedStateDescription, getEvents);
+      }
+    };
+  }
 
   function makeControllerWithStoredState<StoredState>(initialStoredState: StoredState): ControllerWithStoredState<Name, StoredState> {
     return {
@@ -261,88 +261,94 @@ export function makeViewController<Name extends string>(name: Name) {
 
   function makeFinalController<
     StoredState,
-  FullState,
-  Deps extends Dependencies<any, any, any>,
-  EventEmitters extends Record<string, (args: any) => void>,
-  EventList extends Array<Event<string, FullState, StoredState, any>>
-    >(
+    FullState,
+    Deps extends Dependencies<any, any, any>,
+    EventEmitters extends Record<string, (args: any) => void>,
+    EventList extends Array<Event<string, FullState, StoredState, any>>
+  >(
     initialStoredState: StoredState,
     derivedStateAcc: DerivedStateDescription[],
     getEvents: (args: GetEventsArguments<FullState, StoredState>) => EventList,
   ): FinalController<Name, FullState, Deps, EventEmitters> {
 
-    const storedStateStream = new BehaviorSubject(initialStoredState);
+    let dependencies: Deps = {
+      events: {},
+      state: {},
+      view: {},
+    } as any;
 
-    const transformers = derivedStateAcc.map(derivedStateDescription => {
-      const lastDependencies = derivedStateDescription.uses.map(() => Symbol('initial'));
-      return (dependentState: any) => {
-        const dependencies = derivedStateDescription.uses.map(f => f(dependentState));
-        // derivedStateDescription.uses
-        if (dependencies.some((x, index) => lastDependencies[index] !== x)) {
+    return {
+      getViewInterface() {
+        const transformers = derivedStateAcc.map(derivedStateDescription => {
+          const lastDependencies = derivedStateDescription.uses.map(() => Symbol('initial'));
+          return (dependentState: any) => {
+            const dependencies = derivedStateDescription.uses.map(f => f(dependentState));
+            // derivedStateDescription.uses
+            if (dependencies.some((x, index) => lastDependencies[index] !== x)) {
+              return {
+                ...dependentState,
+                [derivedStateDescription.property]: derivedStateDescription.selector(...dependencies),
+              };
+            }
+
+            return dependentState;
+          }
+        })
+        let initialFullState: null | FullState = null;
+
+        const getInitialFullState = (): FullState => {
+          if (initialFullState === null) {
+            initialFullState = transformers.reduce<FullState>((acc, f) => f(acc), initialStoredState as any)
+          }
+          return initialFullState;
+        };
+
+        const rawEventNotificationStream = new Subject<EventNotification<any>>();
+
+        const makeEvent = <Name extends string, Args>(name: Name, handler: EventHandler<FullState, StoredState, Args>): Event<Name, FullState, StoredState, Args> => {
           return {
-            ...dependentState,
-            [derivedStateDescription.property]: derivedStateDescription.selector(...dependencies),
+            name,
+            handler,
+            emit: (args: Args) => rawEventNotificationStream.next({ name, payload: args }),
           };
         }
 
-        return dependentState;
-      }
-    })
-    let initialFullState: null | FullState = null;
+        const events = getEvents({ makeEvent });
+        const eventHanlders = events.reduce<Record<string, EventHandler<FullState, StoredState, any>>>((acc, x) => ({
+          ...acc,
+          [x.name]: x.handler,
+        }), {})
 
-    const getInitialFullState = (): FullState => {
-      if (initialFullState === null) {
-        initialFullState = transformers.reduce<FullState>((acc, f) => f(acc), initialStoredState as any)
-      }
-      return initialFullState;
-    };
-
-    const fullStateStream = concat(
-      defer(() => of(getInitialFullState())),
-      storedStateStream.pipe(
-        o.skip(1),
-        o.map(state => {
-          return transformers.reduce<FullState>((acc, f) => f(acc), state as any);
-        }),
-      ));
-
-    const fullStateForHookStream = fullStateStream.pipe(o.skip(1)); // skip initial state to put it to useState hook
-
-    const rawEventNotificationStream = new Subject<EventNotification<any>>();
-
-    const eventNotificationWithFullStateStream = rawEventNotificationStream.pipe(o.withLatestFrom(fullStateStream));
-
-    eventNotificationWithFullStateStream.subscribe(([notification, state]) => {
-      const nextState = eventHanlders[notification.name](state, notification.payload);
-      if (nextState instanceof Observable) {
-        nextState.subscribe(storedStateStream)
-      } else {
-        storedStateStream.next(nextState)
-      }
-    })
-
-    const makeEvent = <Name extends string, Args>(name: Name, handler: EventHandler<FullState, StoredState, Args>): Event<Name, FullState, StoredState, Args> => {
-      return {
-        name,
-        handler,
-        emit: (args: Args) =>  rawEventNotificationStream.next({ name, payload: args }),
-      };
-    }
-
-    const events = getEvents({ makeEvent });
-    const eventHanlders = events.reduce<Record<string, EventHandler<FullState, StoredState, any>>>((acc, x) => ({
-      ...acc,
-      [x.name]: x.handler,
-    }), {})
-
-    const eventEmitters = events.reduce<EventEmitters>((acc, x) => ({ ...acc, [x.name]: x.emit }), {} as EventEmitters);
-
-    return {
-      getViewInterface () {
+        const eventEmitters = events.reduce<EventEmitters>((acc, x) => ({ ...acc, [x.name]: x.emit }), {} as EventEmitters);
         return {
           useState: () => {
             const [state, setState] = React.useState<FullState>(getInitialFullState())
             React.useEffect(() => {
+              const storedStateStream = new BehaviorSubject(initialStoredState);
+
+              const fullStateStream = concat(
+                defer(() => of(getInitialFullState())),
+                storedStateStream.pipe(
+                  o.skip(1),
+                  o.map(state => {
+                    return transformers.reduce<FullState>((acc, f) => f(acc), state as any);
+                  }),
+                ));
+
+              const fullStateForHookStream = fullStateStream.pipe(o.skip(1)); // skip initial state to put it to useState hook
+
+              const rawEventNotificationStream = new Subject<EventNotification<any>>();
+
+              const eventNotificationWithFullStateStream = rawEventNotificationStream.pipe(o.withLatestFrom(fullStateStream));
+
+              eventNotificationWithFullStateStream.subscribe(([notification, state]) => {
+                const nextState = eventHanlders[notification.name](state, notification.payload);
+                if (nextState instanceof Observable) {
+                  nextState.subscribe(storedStateStream)
+                } else {
+                  storedStateStream.next(nextState)
+                }
+              })
               fullStateForHookStream.subscribe(newState => setState(newState));
             });
             return state;
@@ -353,11 +359,11 @@ export function makeViewController<Name extends string>(name: Name) {
       getParentInterface() {
         return {
           name,
-          initializeDependencies: () => {},
+          initializeDependencies: deps => { },
         }
       }
     }
-    }
+  }
 
   // function makeControllerWithDependencies<D extends Dependencies, FullState>(
   //   derivedStateAcc: any[],
